45696065ed71da86b4b49822b961f8a4f6c3cd39
조성현 <cshcho99@gmail.com>
2024-02-11 14:37:53 +0900

    Change settings

--- atcoder/atcoder0210/B.cpp
+++ atcoder/atcoder0210/B.cpp
@@ -3,17 +3,16 @@ using namespace std;
 #define int long long
 int Q;
 vector<int> A;
-int32_t main(){
-    cin.tie(0)->sync_with_stdio(0);
-    cin>>Q;
-    while(Q--){
-        int a,b;
-        cin>>a>>b;
-        if(a==1){
-            A.push_back(b);
-        }else{
-            cout<<A[A.size()-b]<<'\n';
-        }
+int32_t main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin >> Q;
+  while (Q--) {
+    int a, b;
+    cin >> a >> b;
+    if (a == 1) {
+      A.push_back(b);
+    } else {
+      cout << A[A.size() - b] << '\n';
     }
-
+  }
 }
--- atcoder/atcoder0210/C.cpp
+++ atcoder/atcoder0210/C.cpp
@@ -2,41 +2,40 @@
 using namespace std;
 #define int long long
 int N;
-map<int,int> m;
-int32_t main(){
-    cin.tie(0)->sync_with_stdio(0);
-    cin>>N;
-    m[N]=1;
-    int ans=0;
-    while(!m.empty()){
-        auto iter =m.rbegin();
-        int x=iter->first;
-        int y=iter->second;
-        int a,b;
-        if(x%2==0){
-            a=x/2;
-            b=x/2;
-        }else{
-            a=x/2;
-            b=(x+1)/2;
-        }
-        if(a>1){
-            if(m.find(a)!=m.end()){
-                m[a]=m[a]+y;
-            }else{
-                m[a]=y;
-            }
-        } 
-        if(b>1){
-            if(m.find(b)!=m.end()){
-                m[b]=m[b]+y;
-            }else{
-                m[b]=y;
-            }
-        }
-        m.erase(x); 
-        ans+=x*y;
+map<int, int> m;
+int32_t main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin >> N;
+  m[N] = 1;
+  int ans = 0;
+  while (!m.empty()) {
+    auto iter = m.rbegin();
+    int x = iter->first;
+    int y = iter->second;
+    int a, b;
+    if (x % 2 == 0) {
+      a = x / 2;
+      b = x / 2;
+    } else {
+      a = x / 2;
+      b = (x + 1) / 2;
     }
-    cout<<ans;
-
+    if (a > 1) {
+      if (m.find(a) != m.end()) {
+        m[a] = m[a] + y;
+      } else {
+        m[a] = y;
+      }
+    }
+    if (b > 1) {
+      if (m.find(b) != m.end()) {
+        m[b] = m[b] + y;
+      } else {
+        m[b] = y;
+      }
+    }
+    m.erase(x);
+    ans += x * y;
+  }
+  cout << ans;
 }
