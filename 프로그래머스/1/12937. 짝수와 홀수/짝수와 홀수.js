ed4066c7b94d31ea5b21e249a3496e959bff9675
SeonghyeonJo <cshcho99@gmail.com>
2024-02-22 10:21:54 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -0,0 +1,11 @@
+function solution(num) {
+  //num을  2로 나눈 나머지가 0일때는 "Even" 1일때는 "Odd"
+  return num % 2 === 0 ? 'Even' : 'Odd';
+
+  var answer = '';
+  if (num % 2 === 0) {
+    return 'Even';
+  } else {
+    return 'Odd';
+  }
+}
