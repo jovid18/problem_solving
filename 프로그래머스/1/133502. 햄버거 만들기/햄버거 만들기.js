9e485ca056e11404d3c8c6451441afb2bc795700
SeonghyeonJo <cshcho99@gmail.com>
2024-02-22 11:46:53 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,19 @@
+function solution(ingredient) {
+  let answer = 0;
+  let arr = [];
+  for (let i = 0; i < ingredient.length; ++i) {
+    arr.push(ingredient[i]);
+    if (arr.length >= 4) {
+      let j = arr.length - 1;
+      if (arr[j - 3] === 1 && arr[j - 2] === 2 && arr[j - 1] === 3 && arr[j] === 1) {
+        answer++;
+        arr.pop();
+        arr.pop();
+        arr.pop();
+        arr.pop();
+      }
+    }
+  }
+
+  return answer;
+}
