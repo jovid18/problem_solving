ae20f87d484f7789b8fb1fc495841bd1ffecca90
SeonghyeonJo <cshcho99@gmail.com>
2024-02-22 11:01:35 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.js
@@ -0,0 +1,18 @@
+function solution(number) {
+  // 삼총사
+  // number 배열 중에서 3개 숫자를 더해서 0이 되는 경우의수
+  //
+  let answer = 0;
+
+  //
+  for (let i = 0; i < number.length; i++) {
+    for (let j = i + 1; j < number.length; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        if (number[i] + number[j] + number[k] === 0) {
+          answer++;
+        }
+      }
+    }
+  }
+  return answer;
+}
