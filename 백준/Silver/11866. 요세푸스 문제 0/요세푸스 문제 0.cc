bfd2d5602033c94e15ef75a8fda8acbc9c597142
SEONGHYEON JO <cshcho99@gmail.com>
2024-03-14 13:32:50 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+queue<int> Q;
+int N,K;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin>>N>>K;
+	for(int i=1;i<=N;++i){
+		Q.push(i);
+	}
+	bool first=true;
+	cout<<'<';
+	while(!Q.empty()){
+		for(int i=1;i<K;++i){
+			int a=Q.front();
+			Q.pop();
+			Q.push(a);
+		}
+		if(first){
+			cout<<Q.front();
+			first=false;
+		}else{
+			cout<<", "<<Q.front();
+		}
+		Q.pop();
+	}
+	cout<<'>';
+}
