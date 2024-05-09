778dd5fe1ee0d3fd69345df225f8c087256a0df9
SEONGHYEON JO <cshcho99@gmail.com>
2024-05-10 01:19:34 +0900

    [level 2] Title: 롤케이크 자르기, Time: 8.29 ms, Memory: 44.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+using namespace std;
+int F[1000001], B[1000001];
+bool Fchk[10001], Bchk[10001];
+int solution(vector<int> topping) {
+    int answer = 0;
+    int Fcnt = 0;
+    for (int i = 0; i < topping.size(); ++i) {
+        if (!Fchk[topping[i]]) {
+            Fchk[topping[i]] = true;
+            Fcnt++;
+        }
+        F[i] = Fcnt;
+    }
+    int Bcnt = 0;
+    for (int i = topping.size() - 1; i >= 0; --i) {
+        if (!Bchk[topping[i]]) {
+            Bchk[topping[i]] = true;
+            Bcnt++;
+        }
+        B[i] = Bcnt;
+    }
+    for (int i = 0; i < topping.size() - 1; ++i) {
+        if (F[i] == B[i + 1])
+            answer++;
+    }
+    return answer;
+}
