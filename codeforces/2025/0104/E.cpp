63ceb478e84b5b891ec91d244a499ca88c180a4e
jovid <sh.jo@ecubelabs.com>
2025-01-05 20:30:01 +0900

    fix: 포맷 수정

--- codeforces/2025/0104/E.cpp
+++ codeforces/2025/0104/E.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-
-void solve() {}
-
+int t;
 int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
-    int t;
     cin >> t;
     while (t--) {
-        solve();
     }
 }
