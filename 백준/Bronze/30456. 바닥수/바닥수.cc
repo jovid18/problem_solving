a39483c4fc9c52e80f075a04c5f30cdae8720a8d
Jovid <cshcho99@gmail.com>
2025-02-15 18:30:46 +0900

    [Bronze I] Title: 바닥수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30456. 바닥수/바닥수.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+int B,L;
+int32_t main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin>>B>>L;
+    for(int i=0;i<L-1;++i){
+        cout<<1;
+    }
+    cout<<B;
+}
