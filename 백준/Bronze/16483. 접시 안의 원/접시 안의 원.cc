db3aa3b447223da89ba05fbace09b1465a97435b
Jovid <cshcho99@gmail.com>
2025-02-15 18:18:14 +0900

    [Bronze III] Title: 접시 안의 원, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16483. 접시 안의 원/접시 안의 원.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+const double pi = 3.14159265358979;
+double T;
+
+int main(){
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	cin>>T;
+	cout<<fixed;
+	cout.precision(0);
+	cout<<(T/2)*(T/2);
+}
