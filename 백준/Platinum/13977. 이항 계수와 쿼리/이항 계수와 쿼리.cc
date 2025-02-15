0e1ba6b19edf56583e05c722121474b06e0f53f9
Jovid <cshcho99@gmail.com>
2025-02-15 18:12:44 +0900

    [Platinum V] Title: 이항 계수와 쿼리, Time: 252 ms, Memory: 33272 KB -BaekjoonHub

---
+++ 백준/Platinum/13977. 이항 계수와 쿼리/이항 계수와 쿼리.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <map>
+using namespace std;
+using ll=long long;
+ll fact[4000001]={1};
+ll MOD=1000000007;
+int N;
+int a,b;
+ll power(ll a,ll b){
+	if(b==1){
+		return a%MOD;
+	}
+	if(b%2==0){
+		ll c=power(a,b/2)%MOD;
+		return c*c%MOD;
+	}else{
+		ll c=power(a,(b-1)/2)%MOD;
+		return (c*c%MOD)*a%MOD;
+	}
+}
+
+ll C(int n,int r){
+	ll A=fact[n];
+	ll B=fact[r]*fact[n-r];
+	B%=MOD;
+	ll B2=power(B,MOD-2);
+	ll result=A*B2;
+	result%=MOD;
+	return result;
+}
+int main(){
+	ios_base :: sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	for(ll i=1;i<=4000000;++i){
+		fact[i]=fact[i-1]*i;
+		fact[i]%=MOD;
+	}
+	cin>>N;
+	while(N--){
+		cin>>a>>b;
+		cout<<C(a,b)<<'\n';
+	}
+}
