265c38e9502d37330733cac74de5786619002d52
Jovid <cshcho99@gmail.com>
2025-02-15 17:07:17 +0900

    [Gold II] Title: 트리의 지름, Time: 204 ms, Memory: 9224 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N;
+vector<vector<pair<int,int>>> Tree;
+vector<int> Distance;
+stack<int> Process;
+int Find(int first, vector<int> &Distance){
+	Distance[first]=0;
+	Process.push(first);
+	while(!Process.empty()){
+		int t=Process.top();
+		Process.pop();
+		for(auto e: Tree[t]){
+			if(Distance[e.first]==-1){
+				Distance[e.first]=Distance[t]+e.second;
+				Process.push(e.first);
+			}
+		}
+	}
+	return max_element(Distance.begin(),Distance.end())-Distance.begin();
+}
+
+int main(){
+	cin>>N;
+	Tree.resize(N+1);
+	Distance.assign(N+1,-1);
+	for(int i=1;i<=N;++i){
+		int a;
+		cin>>a;
+		int b,w;
+		while(cin>>b&&b!=-1){
+			cin>>w;
+			Tree[a].push_back({b,w});
+		}
+	}
+	int D1,D2;
+	D1=Find(1,Distance);
+	Distance.assign(N+1,-1);
+	D2=Find(D1,Distance);
+	cout<< *max_element(Distance.begin(),Distance.end());
+}
