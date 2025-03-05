40bd244c15dc812bac8fae1503a3955cb4a4fdf0
jovid <sh.jo@ecubelabs.com>
2025-03-06 04:15:16 +0900

    prefix sum

---
+++ atcoder_training/084.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int, int>
+
+int cnt[5001][5001];
+
+// https://atcoder.jp/contests/joi2012ho/tasks/joi2012ho4
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        z++;
+        cnt[x][y] = max(cnt[x][y], z);
+    }
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cnt[i][j] = max({cnt[i][j], cnt[i - 1][j] - 1, cnt[i - 1][j - 1] - 1});
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (cnt[i][j] > 0) ans++;
+        }
+    }
+    cout << ans << '\n';
+}
